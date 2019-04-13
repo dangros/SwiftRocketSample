5f4d3db2e28b212d0e2d798e0b6bd20fbf204e4c
Dan Gros <dan.gros@gmail.com>
2019-04-13 19:47:54 -0400

    Initial Commit

---
+++ YetAnotherTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YetAnotherTry
+//
+//  Created by TeamGros on 4/13/19.
+//  Copyright © 2019 Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
